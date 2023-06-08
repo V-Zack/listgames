42b794d9590040a9d25588a26127bd162fe36b72
Isaque <isaque7738@gmail.com>
2023-06-08 00:46:29 -0300

    Game seed

--- src/main/java/com/dev/isaque/listgames/entities/Game.java
+++ src/main/java/com/dev/isaque/listgames/entities/Game.java
@@ -27,7 +27,11 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game () {
