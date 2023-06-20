207a1fe599be1eb3138b7a27f58f50d24481ebfd
Isaque <isaque7738@gmail.com>
2023-06-19 23:06:39 -0300

    GET game by id

--- src/main/java/com/dev/isaque/listgames/controllers/GameControler.java
+++ src/main/java/com/dev/isaque/listgames/controllers/GameControler.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dev.isaque.listgames.dto.GameDTO;
 import com.dev.isaque.listgames.dto.GameMinDTO;
-import com.dev.isaque.listgames.entities.Game;
 import com.dev.isaque.listgames.services.GameService;
 
 @RestController
@@ -17,6 +18,12 @@ public class GameControler {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
+		return result;
+	} 
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
---
+++ src/main/java/com/dev/isaque/listgames/dto/GameDTO.java
@@ -0,0 +1,34 @@
+package com.dev.isaque.listgames.dto;
+
+
+import org.springframework.beans.BeanUtils;
+import com.dev.isaque.listgames.entities.Game;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter 
+@Setter
+//@AllArgsConstructor
+@RequiredArgsConstructor
+public class GameDTO {
+	
+	private Long id;
+	private String title;	
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+	
+    //construtor vazio foi suprimido pela anotation @RequiredArgsConstructor 
+	
+	
+    public GameDTO(Game entity) {
+		BeanUtils.copyProperties(entity, this);
+	}
+	
+}
